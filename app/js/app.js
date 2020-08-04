3a18b76b14fda4e4e085c431c0ee6781b51cfa89
Bohdan-Nitro <bogdantrogonsky@gmail.com>
2020-08-04 17:04:31 +0300

    Refactoring mobile(second ver)burger

--- app/js/app.js
+++ app/js/app.js
@@ -15,6 +15,52 @@ $(document).ready(function () {
 
     });
 
+    //Animation configuration
+
+
+    const animItems = document.querySelectorAll("._anim-items");
+
+    if (animItems.length > 0) {
+        window.addEventListener("scroll", animOnScroll);
+        function animOnScroll() {
+            for (let index = 0; index < animItems.length; index++) {
+                const animItem = animItems[index];
+                const animItemHeight = animItem.offsetHeight;
+                const animItemOffset = offset(animItem).top;
+                const animStart = 4;
+
+                let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+                if (animItemHeight > window.innerHeight) {
+                    animItemPoint = window.innerHeight - window.innerHeight/ animStart;
+                }
+
+                if ((pageXOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                    animItem.classList.add("_active");
+                } else  {
+                    if (!animItem.classList.contains("_anim-no-hide")) {
+                        animItem.classList.remove("_active");
+                    }
+                }
+
+            }
+        }
+
+        function offset(el) {
+            const rect = el.getBoundingClientRect(),
+                scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+                scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
+        }
+        setTimeout(() => {
+
+        }, 300);
+        animOnScroll();
+    }
+
+
+
+
 
 
     /////////////////////////////////////////////////////////
@@ -54,7 +100,7 @@ $(document).ready(function () {
     });
     btn.on("click", function (e) {
         e.preventDefault();
-        $('body, html').animate({scrollTop: 0}, 500);
+        $('body, html').animate({scrollTop: 0}, 800);
     });
 
     //Прокрутка навверх страницы
@@ -136,29 +182,29 @@ $(document).ready(function () {
         }
     });
 
-
-    let animation = anime({
-        targets: '.hero',
-        translateX: -5,
-        autoplay: true,
-        opacity: [0, 1],
-        easing: 'easeOutExpo',
-        duration: 2000,
-        delay: 600
-
-
-    });
-
-    let textAnime = anime({
-        targets: '.offer-count',
-        translateX: 5,
-        autoplay: true,
-        opacity: [0, 1],
-        duration: 2000,
-        easing: 'easeOutExpo',
-        delay: 300
-
-    });
+    //
+    // let animation = anime({
+    //     targets: '.hero',
+    //     translateX: -5,
+    //     autoplay: true,
+    //     opacity: [0, 1],
+    //     easing: 'easeOutExpo',
+    //     duration: 2000,
+    //     delay: 600
+    //
+    //
+    // });
+    //
+    // let textAnime = anime({
+    //     targets: '.offer-count',
+    //     translateX: 5,
+    //     autoplay: true,
+    //     opacity: [0, 1],
+    //     duration: 2000,
+    //     easing: 'easeOutExpo',
+    //     delay: 300
+    //
+    // });
 
 
 });
